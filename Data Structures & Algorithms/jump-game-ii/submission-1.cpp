6f1678dcb0817d7115c934ae44937053802ecf81
Axit Tiwari <axittiwari0@gmail.com>
2026-06-22 20:33:44 +0530

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        
+        // BFS:
+        
+        int n = nums.size() - 1;
+
+        int l = 0, r = 0;
+        int res = 0;
+
+        while (r < n) {
+
+            int farthest = 0;
+            for (int i = l; i <= r; i++) {
+                farthest = max(farthest, i + nums[i]);
+            }
+
+            l = r + 1;
+            r = farthest;
+
+            res += 1;
+        }
+        return res;
+    }
+};
